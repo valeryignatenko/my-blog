c98db70ed7e4afb349485099ebae9f3238a6bef6
valeryignatenko <96315327+valeryignatenko@users.noreply.github.com>
2024-02-18 21:38:00 +0300

    Update ThemeToggle.tsx

--- src/layouts/RootLayout/Header/ThemeToggle.tsx
+++ src/layouts/RootLayout/Header/ThemeToggle.tsx
@@ -14,7 +14,7 @@ const ThemeToggle: React.FC<Props> = () => {
 
   return (
     <StyledWrapper onClick={handleClick}>
-      <Emoji>{scheme === "light" ? "☀️" : "🌙"}</Emoji>
+      <Emoji>{scheme === "light" ? "🌕" : "🌑"}</Emoji>
     </StyledWrapper>
   )
 }
